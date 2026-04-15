5b70868f5e317c946c689d1fb59eef1322c8fa3e
Ravindra pal singh <ravindrapalsingh158@gmail.com>
2026-04-15 10:47:53 +0530

    some changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Basic validations
   validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_15_043820) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_15_050320) do
   create_table "admin_users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -85,12 +85,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_15_043820) do
     t.datetime "created_at", null: false
     t.date "date_of_birth"
     t.string "email"
+    t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
     t.integer "marks"
     t.string "phone_number"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.string "role"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
 
   add_foreign_key "student_projects", "projects"
